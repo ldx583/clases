e08351b3e710ab263fb46e8c3efbbbae41e0494b
ldx583 <ldxsoria@gmail.com>
2020-01-18 23:32:32 -0500

    commit

---
+++ app/src/main/java/local/sanjose/inventario/Activities/SE_CREO_SOLO.kt
@@ -0,0 +1,11 @@
+package local.sanjose.inventario.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SE_CREO_SOLO : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
