01b8dd0099bbdd35f63217c7c5dad2d24f7e2684
ldx583 <ldxsoria@gmail.com>
2020-01-19 01:17:54 -0500

    commit

--- app/src/main/java/local/sanjose/inventario/Activities/ClickEventsActivity.kt
+++ app/src/main/java/local/sanjose/inventario/Activities/ClickEventsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.widget.Toast.*
 import local.sanjose.inventario.R
 
 class ClickEventsActivity : AppCompatActivity(), View.OnLongClickListener {
@@ -32,23 +33,20 @@ class ClickEventsActivity : AppCompatActivity(), View.OnLongClickListener {
     //El metodo tiene que ser publico
     //Los nombres tiene que coincidir y recibe 1 solo parametro (view)
     fun xmlClick(view: View){
-        Toast.makeText(this,"Click by XML!", Toast.LENGTH_LONG).show()
+        makeText(this,"Click by XML!", LENGTH_LONG).show()
     }
 
     private fun clickInLine(){
         val btn = findViewById<Button>(R.id.btnClickInLine)
-        btn.setOnClickListener { Toast.makeText(this, "Clic in Line!", Toast.LENGTH_LONG).show() }
+        btn.setOnClickListener { makeText(this, "Clic in Line!", LENGTH_LONG).show() }
     }
 
     override fun onLongClick(view: View): Boolean {
         when(view.id){
-           R.id.btnClickMulti1 -> Toast.makeText(this,"Click Multi 1!", Toast.LENGTH_LONG).show()
-           R.id.btnClickMulti2 -> Toast.makeText(this,"Click Multi 2!", Toast.LENGTH_LONG).show()
-           R.id.btnClickMulti3 -> Toast.makeText(this,"Click Multi 3!", Toast.LENGTH_LONG).show()
+           R.id.btnClickMulti1 -> makeText(this,"Click Multi 1!", LENGTH_LONG).show()
+           R.id.btnClickMulti2 -> makeText(this,"Click Multi 2!", LENGTH_LONG).show()
+           R.id.btnClickMulti3 -> makeText(this,"Click Multi 3!", LENGTH_LONG).show()
         }
         return true
     }
-
-
-
 }
