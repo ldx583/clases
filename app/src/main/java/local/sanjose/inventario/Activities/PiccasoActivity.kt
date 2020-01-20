2719a4caca244c3d871b036911db5af43e84e69b
ldx583 <ldxsoria@gmail.com>
2020-01-20 15:00:25 -0500

    commit

--- app/src/main/java/local/sanjose/inventario/Activities/PiccasoActivity.kt
+++ app/src/main/java/local/sanjose/inventario/Activities/PiccasoActivity.kt
@@ -1,7 +1,9 @@
 package local.sanjose.inventario.Activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import local.sanjose.inventario.R
 import kotlinx.android.synthetic.main.activity_piccaso.*
@@ -19,12 +21,12 @@ class PiccasoActivity : AppCompatActivity() {
     }
 
         private fun loadImages(){
-            Picasso
+            /*Picasso
                 .with(this)
                 .load("https://images.pexels.com/photos/3330175/pexels-photo-3330175.jpeg")
                 .fit()
                 .transform(CircleTransform())
-                .into(imageViewTop)
+                .into(imageViewTop)*/
 
             Picasso
                 .with(this)
@@ -38,6 +40,26 @@ class PiccasoActivity : AppCompatActivity() {
                 .load("https://images.pexels.com/photos/3493772/pexels-photo-3493772.jpeg")
                 .fit() //Usar tsodo el espacio
                 .into(imageViewBottom2)
+
+            val context: Context = this
+            Picasso
+                .with(this)
+                .load("https://images.pexels.com/photos/3283090/pexels-photo-3283090.jpeg")
+                .fetch(object: Callback{
+                    override fun onSuccess() {
+                        imageViewTop.alpha = 0f
+                        Picasso.with(context)
+                            .load("https://images.pexels.com/photos/3283090/pexels-photo-3283090.jpeg")
+                            .fit()
+                            .transform(CircleTransform())
+                            .into(imageViewTop)
+                        imageViewTop.animate().setDuration(1300).alpha(1f).start()
+                    }
+
+                    override fun onError() {
+                    }
+
+                })
 
         }
 
